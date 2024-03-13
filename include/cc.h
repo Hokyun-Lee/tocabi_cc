3802bf5d5db6e60ffb3d25c06f2ece0018d55454
Hokyun-Lee <lhg0805@gmail.com>
2024-03-13 18:26:14 +0900

    branch generate

--- include/cc.h
+++ include/cc.h
@@ -1,12 +1,10 @@
 #include "tocabi_lib/robot_data.h"
 #include "wholebody_functions.h"
-#include "geometry_msgs/Pose.h"
-#include "geometry_msgs/Vector3.h"
-#include <tf/tf.h>
-#include <tf2/LinearMath/Quaternion.h>
-#include <Eigen/Dense>
-#include <Eigen/Sparse>
+#include <random>
+#include <cmath>
 
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
 
 class CustomController
 {
@@ -20,31 +18,99 @@ public:
     void computeFast();
     void computePlanner();
     void copyRobotData(RobotData &rd_l);
-    void PublishHapticData();
-
-    void HapticPoseCallback(const geometry_msgs::PoseConstPtr &msg);
-    Eigen::Matrix3d Quat2rotmatrix(double q0, double q1, double q2, double q3);
-    float PositionMapping( float haptic_pos, int i);
 
     RobotData &rd_;
     RobotData rd_cc_;
 
-    ros::NodeHandle nh_cc_;
-    ros::CallbackQueue queue_cc_;
-    ros::Subscriber haptic_pose_sub_;
-    ros::Publisher haptic_force_pub_;
+    //////////////////////////////////////////// Donghyeon RL /////////////////////////////////////////
+    void loadNetwork();
+    void processNoise();
+    void processObservation();
+    void feedforwardPolicy();
+    void initVariable();
+    Eigen::Vector3d mat2euler(Eigen::Matrix3d mat);
+
+    static const int num_action = 13;
+    static const int num_actuator_action = 12;
+    static const int num_cur_state = 50;
+    static const int num_cur_internal_state = 37;
+    static const int num_state_skip = 2;
+    static const int num_state_hist = 5;
+    static const int num_state = num_cur_internal_state*num_state_hist+num_action*(num_state_hist-1);
+    static const int num_hidden = 256;
+
+    Eigen::MatrixXd policy_net_w0_;
+    Eigen::MatrixXd policy_net_b0_;
+    Eigen::MatrixXd policy_net_w2_;
+    Eigen::MatrixXd policy_net_b2_;
+    Eigen::MatrixXd action_net_w_;
+    Eigen::MatrixXd action_net_b_;
+    Eigen::MatrixXd hidden_layer1_;
+    Eigen::MatrixXd hidden_layer2_;
+    Eigen::MatrixXd rl_action_;
+
+    Eigen::MatrixXd value_net_w0_;
+    Eigen::MatrixXd value_net_b0_;
+    Eigen::MatrixXd value_net_w2_;
+    Eigen::MatrixXd value_net_b2_;
+    Eigen::MatrixXd value_net_w_;
+    Eigen::MatrixXd value_net_b_;
+    Eigen::MatrixXd value_hidden_layer1_;
+    Eigen::MatrixXd value_hidden_layer2_;
+    double value_;
+
+    bool stop_by_value_thres_ = false;
+    Eigen::Matrix<double, MODEL_DOF, 1> q_stop_;
+    float stop_start_time_;
     
-    Eigen::Vector3d haptic_pos_;
-    Eigen::Vector4d haptic_ori_;
-    Eigen::Matrix3d haptic_orientation_;
+    Eigen::MatrixXd state_;
+    Eigen::MatrixXd state_cur_;
+    Eigen::MatrixXd state_buffer_;
+    Eigen::MatrixXd state_mean_;
+    Eigen::MatrixXd state_var_;
 
-    //WholebodyController &wbc_;
-    //TaskCommand tc;
+    std::ofstream writeFile;
 
-    double haptic_force_[3];
+    float phase_ = 0.0;
 
-    //for HK
+    bool is_on_robot_ = false;
+    bool is_write_file_ = true;
+    Eigen::Matrix<double, MODEL_DOF, 1> q_dot_lpf_;
 
+    Eigen::Matrix<double, MODEL_DOF, 1> q_init_;
+    Eigen::Matrix<double, MODEL_DOF, 1> q_noise_;
+    Eigen::Matrix<double, MODEL_DOF, 1> q_noise_pre_;
+    Eigen::Matrix<double, MODEL_DOF, 1> q_vel_noise_;
+
+    Eigen::Matrix<double, MODEL_DOF, 1> torque_init_;
+    Eigen::Matrix<double, MODEL_DOF, 1> torque_spline_;
+    Eigen::Matrix<double, MODEL_DOF, 1> torque_rl_;
+    Eigen::Matrix<double, MODEL_DOF, 1> torque_bound_;
+
+    Eigen::Matrix<double, MODEL_DOF, MODEL_DOF> kp_;
+    Eigen::Matrix<double, MODEL_DOF, MODEL_DOF> kv_;
+
+    float start_time_;
+    float time_inference_pre_ = 0.0;
+    float time_write_pre_ = 0.0;
+
+    double time_cur_;
+    double time_pre_;
+    double action_dt_accumulate_ = 0.0;
+
+    Eigen::Vector3d euler_angle_;
+
+    // float ft_left_init_ = 500.0;
+    // float ft_right_init_ = 500.0;
+
+    // Joystick
+    ros::NodeHandle nh_;
+
+    void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+    ros::Subscriber joy_sub_;
+
+    double target_vel_x_ = 0.0;
+    double target_vel_y_ = 0.0;
 
 private:
     Eigen::VectorQd ControlVal_;
